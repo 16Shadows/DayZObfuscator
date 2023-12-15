f9cdda7be6c2d8af20e9f608034d892d3ccf7b0e
16Shadows <egor.0.00@mail.ru>
2023-12-15 16:33:22 +0500

    Base class for project parsing.

---
+++ DayZObfuscatorModel/Analyzers/ProjectFolderAnalyzer.cs
@@ -0,0 +1,15 @@
+﻿using DayZObfuscatorModel.PBO;
+
+namespace DayZObfuscatorModel.Analyzers
+{
+	public static class ProjectFolderAnalyzer
+	{
+		public static IEnumerable<PBODescriptor> Analyze(string path)
+		{
+			if (!Path.Exists(path) || Path.GetFileName(path) != null)
+				throw new ArgumentException("Path should be a valid path to a directory.", nameof(path));
+
+			return null;
+		}
+	}
+}
