fd301626cd18bddc6f6e41988bab8cd6d511553c
16Shadows <egor.0.00@mail.ru>
2023-12-15 16:32:06 +0500

    DynamicRingBuffer - a dynamically resizable ring buffer to be used in InputReader of a parser.

---
+++ DayZObfuscatorModel/DynamicRingBuffer.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace DayZObfuscatorModel
+{
+	/// <summary>
+	/// A ring buffer which supports dynamic expansion of its size
+	/// </summary>
+	/// <typeparam name="T">The type to store in ring buffer</typeparam>
+	public class DynamicRingBuffer<T> : IEnumerable<T>
+	{
+		private T[] _Buffer;
+		private int _ReadIndex;
+		private int _WriteIndex;
+		private int _BufferStartIndex;
+		private int _Count;
+		private int _Capacity;
+
+		public int Capacity => _Capacity;
+		public int Count => _Count;
+		public bool IsReadOnly => false;
+		public bool IsSynchronized => false;
+		public object SyncRoot { get; } = new object();
+
+		public DynamicRingBuffer(int capacity)
+		{
+			if (capacity < 1)
+				throw new ArgumentOutOfRangeException(nameof(capacity), $"{nameof(capacity)} should be greater than 0.");
+
+			_Buffer = new T[capacity];
+			_ReadIndex = 0;
+			_WriteIndex = 0;
+			_Capacity = capacity;
+			_Count = 0;
+			_BufferStartIndex = 0;
+		}
+
+		public DynamicRingBuffer(IEnumerable<T> items) : this(1)
+		{
+			AddRange(items);
+		}
+
+		public DynamicRingBuffer(params T[] items) : this((IEnumerable<T>)items) {}
+
+		/// <summary>
+		/// Reads or writes stringbuffer at specified index starting from index of the oldest value
+		/// </summary>
+		/// <param name="index"></param>
+		/// <returns></returns>
+		public T this[int index]
+		{
+			get
+			{
+				if (Count == 0)
+					throw new InvalidOperationException($"{nameof(DynamicRingBuffer<T>)} is empty.");
+
+				return _Buffer[AdjustReadIndex(index)];
+			}
+			set
+			{
+				if (Count == 0)
+					throw new InvalidOperationException($"{nameof(DynamicRingBuffer<T>)} is empty.");
+
+				_Buffer[AdjustReadIndex(index)] = value;
+			}
+		}
+
+		/// <summary>
+		/// Writes to the ring buffer. If it is full, overwrites the oldest value.
+		/// </summary>
+		/// <param name="value">The value to write.</param>
+		public void Write(T value)
+		{
+			if (Count < Capacity)
+			{
+				_Count++;
+				_Buffer[AdjustBufferIndex(_WriteIndex++)] = value;
+			}
+			else
+			{
+				_Buffer[ AdjustWriteIndex(0) ] = value;
+				_WriteIndex = AdjustWriteIndex(1);
+				_ReadIndex = AdjustReadIndex(1);
+			}
+		}
+
+		/// <summary>
+		/// Writes a sequence of elements to the ring buffer. If it is full or becomes, overwrites values starting from the oldest.
+		/// </summary>
+		/// <param name="values"></param>
+		public void WriteRange(IEnumerable<T> values)
+		{
+			foreach (T value in values)
+				Write(value);
+		}
+
+		public void WriteRange(params T[] values) => WriteRange((IEnumerable<T>)values);
+
+		/// <summary>
+		/// Pushes a value into the ring buffer. If it is full, expands the ring buffer.
+		/// </summary>
+		/// <param name="value"></param>
+		public void Add(T value)
+		{
+			if (Count >= Capacity)
+				ExpandBuffer();
+
+			Write(value);
+		}
+
+		public void AddRange(IEnumerable<T> values)
+		{
+			foreach (T value in values)
+				Add(value);
+		}
+
+		public void AddRange(params T[] values) => AddRange((IEnumerable<T>)values);
+
+		public T Pop()
+		{
+			T item = this[0];
+			_ReadIndex = AdjustReadIndex(1);
+			_Count--;
+			return item;
+		}
+
+		public IEnumerable<T> Pop(int count)
+		{
+			T[] result = new T[count];
+			int index = 0;
+			while (count-- > 0)
+				result[index++] = Pop();
+
+			return result;
+		}
+
+		public IEnumerator<T> GetEnumerator()
+		{
+			for (int i = 0; i < Count; i++)
+				yield return this[i];
+			yield break;
+		}
+
+		IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+		private void ExpandBuffer()
+		{
+			_Capacity++;
+			if (_Capacity <= _Buffer.Length)
+				return;
+
+			double expansionCoef = _Buffer.Length < 256 ? 2 :
+									_Buffer.Length < 1024 ? 1.2 : 1.05;
+
+			T[] newBuffer = new T[(int)Math.Ceiling(_Buffer.Length * expansionCoef)];
+			int index = 0;
+			foreach (T value in this)
+				newBuffer[index++] = value;
+
+			_Buffer = newBuffer;
+			_BufferStartIndex = 0;
+			_ReadIndex = 0;
+			_WriteIndex = AdjustWriteIndex(index);
+		}
+
+		private int AdjustReadIndex(int index) => AdjustBufferIndex(_ReadIndex + index % _Count);
+		private int AdjustWriteIndex(int index) => AdjustBufferIndex(_WriteIndex + index % _Count);
+		private int AdjustBufferIndex(int index) => (_BufferStartIndex + index % _Capacity) % _Buffer.Length;
+
+		public void Clear()
+		{
+			_BufferStartIndex = 0;
+			_WriteIndex = 0;
+			_ReadIndex = 0;
+			_Count = 0;
+		}
+
+		public bool Contains(T item)
+		{
+			foreach (T value in this)
+				if (EqualityComparer<T>.Default.Equals(value, item))
+					return true;
+
+			return false;
+		}
+	}
+}
---
+++ Tests_DayZObfuscatorModel/DynamicRingBuffer_Tests.cs
@@ -0,0 +1,443 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DayZObfuscatorModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http.Headers;
+using System.Buffers;
+
+namespace DayZObfuscatorModel.Tests
+{
+	[TestClass()]
+	public class DynamicRingBuffer_Tests
+	{
+		[TestMethod()]
+		public void DefaultConstructor_Test()
+		{
+			DynamicRingBuffer<int> buffer = new DynamicRingBuffer<int>();
+
+			Assert.AreEqual(0, buffer.Count);
+			Assert.ThrowsException<InvalidOperationException>(() => buffer[0]);
+		}
+
+		[TestMethod()]
+		public void CapacityConstructor_Test()
+		{
+			DynamicRingBuffer<int> buffer = new DynamicRingBuffer<int>(15);
+
+			Assert.AreEqual(15, buffer.Capacity);
+			Assert.AreEqual(0, buffer.Count);
+			Assert.ThrowsException<InvalidOperationException>(() => buffer[0]);
+		}
+
+		[TestMethod()]
+		public void InvalidCapacityConstructor_Test()
+		{
+			Assert.ThrowsException<ArgumentOutOfRangeException>(() =>
+			{
+				DynamicRingBuffer<int> buffer = new DynamicRingBuffer<int>(-15);
+			});
+		}
+
+		[TestMethod()]
+		public void EnumerableConstructor_Test()
+		{
+			int[] arr = new int[3] { 1, 2, 3 };
+
+			DynamicRingBuffer<int> buffer = new DynamicRingBuffer<int>(arr);
+
+			Assert.AreEqual(3, buffer.Capacity);
+			Assert.AreEqual(3, buffer.Count);
+			for (int i = 0; i < arr.Length; i++)
+				Assert.AreEqual(arr[i], buffer[i]);
+		}
+
+		[TestMethod()]
+		public void ParamsArrayConstructor_Test()
+		{
+			int[] arr = new int[] { 1, 2, 3 };
+			DynamicRingBuffer<int> buffer = new DynamicRingBuffer<int>(1, 2, 3);
+
+			Assert.AreEqual(3, buffer.Capacity);
+			Assert.AreEqual(3, buffer.Count);
+			for (int i = 0; i < arr.Length; i++)
+				Assert.AreEqual(arr[i], buffer[i]);
+		}
+
+		[TestMethod()]
+		public void WriteOverCapacity_Test()
+		{
+			DynamicRingBuffer<int> buffer = new DynamicRingBuffer<int>( 2, 3, 4, 5 );
+
+			Assert.AreEqual(4, buffer.Capacity);
+			Assert.AreEqual(4, buffer.Count);
+			Assert.AreEqual(2, buffer[0]);
+			Assert.AreEqual(3, buffer[1]);
+			Assert.AreEqual(4, buffer[2]);
+			Assert.AreEqual(5, buffer[3]);
+
+			buffer.Write(16);
+
+			Assert.AreEqual(4, buffer.Capacity);
+			Assert.AreEqual(4, buffer.Count);
+			Assert.AreEqual(3, buffer[0]);
+			Assert.AreEqual(4, buffer[1]);
+			Assert.AreEqual(5, buffer[2]);
+			Assert.AreEqual(16, buffer[3]);
+		}
+
+		[TestMethod()]
+		public void WriteUnderCapacity_Test()
+		{
+			DynamicRingBuffer<int> buffer = new DynamicRingBuffer<int>( 4 );
+
+			buffer.Write(2);
+			buffer.Write(3);
+			buffer.Write(4);
+
+			Assert.AreEqual(4, buffer.Capacity);
+			Assert.AreEqual(3, buffer.Count);
+			Assert.AreEqual(2, buffer[0]);
+			Assert.AreEqual(3, buffer[1]);
+			Assert.AreEqual(4, buffer[2]);
+
+			buffer.Write(16);
+
+			Assert.AreEqual(4, buffer.Capacity);
+			Assert.AreEqual(4, buffer.Count);
+			Assert.AreEqual(2, buffer[0]);
+			Assert.AreEqual(3, buffer[1]);
+			Assert.AreEqual(4, buffer[2]);
+			Assert.AreEqual(16, buffer[3]);
+		}
+
+		[TestMethod()]
+		public void WriteRangeEnumerable_Test()
+		{
+			DynamicRingBuffer<int> buffer = new DynamicRingBuffer<int>(2, 3, 4, 5);
+
+			Assert.AreEqual(4, buffer.Capacity);
+			Assert.AreEqual(4, buffer.Count);
+			Assert.AreEqual(2, buffer[0]);
+			Assert.AreEqual(3, buffer[1]);
+			Assert.AreEqual(4, buffer[2]);
+			Assert.AreEqual(5, buffer[3]);
+
+			int[] arr = new int[] { 16, 23 };
+			buffer.WriteRange(arr);
+
+			Assert.AreEqual(4, buffer.Capacity);
+			Assert.AreEqual(4, buffer.Count);
+			Assert.AreEqual(4, buffer[0]);
+			Assert.AreEqual(5, buffer[1]);
+			Assert.AreEqual(16, buffer[2]);
+			Assert.AreEqual(23, buffer[3]);	
+		}
+
+		[TestMethod()]
+		public void WriteRangeParams_Test()
+		{
+			DynamicRingBuffer<int> buffer = new DynamicRingBuffer<int>(2, 3, 4, 5);
+
+			Assert.AreEqual(4, buffer.Capacity);
+			Assert.AreEqual(4, buffer.Count);
+			Assert.AreEqual(2, buffer[0]);
+			Assert.AreEqual(3, buffer[1]);
+			Assert.AreEqual(4, buffer[2]);
+			Assert.AreEqual(5, buffer[3]);
+
+			buffer.WriteRange(16, 23);
+
+			Assert.AreEqual(4, buffer.Capacity);
+			Assert.AreEqual(4, buffer.Count);
+			Assert.AreEqual(4, buffer[0]);
+			Assert.AreEqual(5, buffer[1]);
+			Assert.AreEqual(16, buffer[2]);
+			Assert.AreEqual(23, buffer[3]);
+		}
+
+		[TestMethod()]
+		public void AddUnderCapacity_Test()
+		{
+			DynamicRingBuffer<int> buffer = new DynamicRingBuffer<int>(6);
+
+			buffer.WriteRange(2, 3, 4, 5);
+
+			Assert.AreEqual(6, buffer.Capacity);
+			Assert.AreEqual(4, buffer.Count);
+			Assert.AreEqual(2, buffer[0]);
+			Assert.AreEqual(3, buffer[1]);
+			Assert.AreEqual(4, buffer[2]);
+			Assert.AreEqual(5, buffer[3]);
+
+			buffer.Add(17);
+
+			Assert.AreEqual(6, buffer.Capacity);
+			Assert.AreEqual(5, buffer.Count);
+			Assert.AreEqual(2, buffer[0]);
+			Assert.AreEqual(3, buffer[1]);
+			Assert.AreEqual(4, buffer[2]);
+			Assert.AreEqual(5, buffer[3]);
+			Assert.AreEqual(17, buffer[4]);
+		}
+
+		[TestMethod()]
+		public void AddOverCapacity_Test()
+		{
+			DynamicRingBuffer<int> buffer = new DynamicRingBuffer<int>(4);
+
+			buffer.WriteRange(2, 3, 4, 5);
+
+			Assert.AreEqual(4, buffer.Capacity);
+			Assert.AreEqual(4, buffer.Count);
+			Assert.AreEqual(2, buffer[0]);
+			Assert.AreEqual(3, buffer[1]);
+			Assert.AreEqual(4, buffer[2]);
+			Assert.AreEqual(5, buffer[3]);
+
+			buffer.Add(17);
+
+			Assert.AreEqual(5, buffer.Capacity);
+			Assert.AreEqual(5, buffer.Count);
+			Assert.AreEqual(2, buffer[0]);
+			Assert.AreEqual(3, buffer[1]);
+			Assert.AreEqual(4, buffer[2]);
+			Assert.AreEqual(5, buffer[3]);
+			Assert.AreEqual(17, buffer[4]);
+		}
+
+		[TestMethod()]
+		public void AddOverCapacityLarge_Test()
+		{
+			DynamicRingBuffer<int> buffer = new DynamicRingBuffer<int>(4);
+
+			buffer.WriteRange(2, 3, 4, 5);
+
+			Assert.AreEqual(4, buffer.Capacity);
+			Assert.AreEqual(4, buffer.Count);
+			Assert.AreEqual(2, buffer[0]);
+			Assert.AreEqual(3, buffer[1]);
+			Assert.AreEqual(4, buffer[2]);
+			Assert.AreEqual(5, buffer[3]);
+
+			for (int i = 0; i < 20; i++)
+				buffer.Add(17);
+
+			Assert.AreEqual(24, buffer.Capacity);
+			Assert.AreEqual(24, buffer.Count);
+			Assert.AreEqual(2, buffer[0]);
+			Assert.AreEqual(3, buffer[1]);
+			Assert.AreEqual(4, buffer[2]);
+			Assert.AreEqual(5, buffer[3]);
+			for (int i = 0; i < 20; i++)
+				Assert.AreEqual(17, buffer[4+i]);
+		}
+
+		[TestMethod()]
+		public void GetEnumeratorT_Test()
+		{
+			int[] arr = new int[5] { 4, 4, 3, 2, 1 };
+			DynamicRingBuffer<int> buffer = new DynamicRingBuffer<int>(arr);
+
+			Assert.AreEqual(5, buffer.Capacity);
+			Assert.AreEqual(5, buffer.Count);
+			int index = 0;
+			foreach (int item in buffer)
+				Assert.AreEqual(arr[index++], item);
+		}
+
+		[TestMethod()]
+		public void GetEnumerator_Test()
+		{
+			object[] arr = new object[5] { 4, 4, 3, 2, 1 };
+			DynamicRingBuffer<int> buffer = new DynamicRingBuffer<int>(arr.Select(x => (int)x));
+
+			Assert.AreEqual(5, buffer.Capacity);
+			Assert.AreEqual(5, buffer.Count);
+			
+			int index = 0;
+			foreach (object item in (System.Collections.IEnumerable)buffer)
+				Assert.AreEqual(arr[index++], item);
+		}
+
+		[TestMethod()]
+		public void AddRangeEnumerable_Test()
+		{
+			int[] arr = new int[] { 4, 2, 3 };
+			DynamicRingBuffer<int> buffer = new(3);
+
+			Assert.AreEqual(3, buffer.Capacity);
+			Assert.AreEqual(0, buffer.Count);
+
+			buffer.AddRange(arr);
+
+			Assert.AreEqual(3, buffer.Capacity);
+			Assert.AreEqual(3, buffer.Count);
+			for (int i = 0; i < arr.Length; i++)
+				Assert.AreEqual(arr[i], buffer[i]);
+		}
+
+		[TestMethod()]
+		public void AddRangeParams_Test()
+		{
+			int[] arr = new int[] { 4, 2, 3 };
+			DynamicRingBuffer<int> buffer = new(3);
+
+			Assert.AreEqual(3, buffer.Capacity);
+			Assert.AreEqual(0, buffer.Count);
+
+			buffer.AddRange(4, 2, 3);
+
+			Assert.AreEqual(3, buffer.Capacity);
+			Assert.AreEqual(3, buffer.Count);
+			for (int i = 0; i < arr.Length; i++)
+				Assert.AreEqual(arr[i], buffer[i]);
+		}
+
+		[TestMethod()]
+		public void Pop_Test()
+		{
+			int[] arr = new int[] { 4, 2, 3 };
+			DynamicRingBuffer<int> buffer = new(arr);
+
+			Assert.AreEqual(3, buffer.Capacity);
+			Assert.AreEqual(3, buffer.Count);
+
+			for (int i = 0; i < arr.Length; i++)
+				Assert.AreEqual(arr[i], buffer[i]);
+
+			Assert.AreEqual(4, buffer.Pop());
+
+			Assert.AreEqual(3, buffer.Capacity);
+			Assert.AreEqual(2, buffer.Count);
+			for (int i = 0; i < arr.Length - 1; i++)
+				Assert.AreEqual(arr[i+1], buffer[i]);
+		}
+
+		[TestMethod()]
+		public void PopAdd_Test()
+		{
+			int[] arr = new int[] { 4, 2, 3 };
+			int[] arr2 = new int[] { 2, 3, 5 };
+			DynamicRingBuffer<int> buffer = new(arr);
+
+			Assert.AreEqual(3, buffer.Capacity);
+			Assert.AreEqual(3, buffer.Count);
+
+			for (int i = 0; i < arr.Length; i++)
+				Assert.AreEqual(arr[i], buffer[i]);
+
+			Assert.AreEqual(4, buffer.Pop());
+			buffer.Add(5);
+
+			Assert.AreEqual(3, buffer.Capacity);
+			Assert.AreEqual(3, buffer.Count);
+			for (int i = 0; i < arr2.Length; i++)
+				Assert.AreEqual(arr2[i], buffer[i]);
+		}
+
+		[TestMethod()]
+		public void PopN_Test()
+		{
+			int[] arr = new int[] { 4, 2, 3 };
+			DynamicRingBuffer<int> buffer = new(arr);
+
+			Assert.AreEqual(3, buffer.Capacity);
+			Assert.AreEqual(3, buffer.Count);
+
+			for (int i = 0; i < arr.Length; i++)
+				Assert.AreEqual(arr[i], buffer[i]);
+
+			int index = 0;
+			foreach (int item in buffer.Pop(3))
+				Assert.AreEqual(arr[index++], item);
+
+			Assert.AreEqual(3, buffer.Capacity);
+			Assert.AreEqual(0, buffer.Count);
+		}
+
+		[TestMethod()]
+		public void Clear_Test()
+		{
+			int[] arr = new int[] { 4, 2, 3 };
+			DynamicRingBuffer<int> buffer = new(arr);
+
+			Assert.AreEqual(3, buffer.Capacity);
+			Assert.AreEqual(3, buffer.Count);
+
+			for (int i = 0; i < arr.Length; i++)
+				Assert.AreEqual(arr[i], buffer[i]);
+
+			buffer.Clear();
+
+			Assert.AreEqual(3, buffer.Capacity);
+			Assert.AreEqual(0, buffer.Count);
+
+			buffer.AddRange(arr);
+
+			Assert.AreEqual(3, buffer.Capacity);
+			Assert.AreEqual(3, buffer.Count);
+
+			for (int i = 0; i < arr.Length; i++)
+				Assert.AreEqual(arr[i], buffer[i]);
+		}
+
+		[TestMethod()]
+		public void Contains_Test()
+		{
+			int[] arr = new int[] { 4, 2, 3 };
+			DynamicRingBuffer<int> buffer = new(arr);
+
+			Assert.AreEqual(3, buffer.Capacity);
+			Assert.AreEqual(3, buffer.Count);
+
+			for (int i = 0; i < arr.Length; i++)
+				Assert.AreEqual(arr[i], buffer[i]);
+
+			Assert.IsTrue(buffer.Contains(2));
+			Assert.IsFalse(buffer.Contains(15));
+
+			buffer[1] = 0;
+
+			Assert.IsFalse(buffer.Contains(2));
+		}
+
+		[TestMethod()]
+		public void IndexerGet_Test()
+		{
+			int[] arr = new int[] { 4, 2, 3 };
+			DynamicRingBuffer<int> buffer = new(arr);
+
+			Assert.AreEqual(2, arr[1]);
+		}
+
+		[TestMethod()]
+		public void IndexerInvalidGet_Test()
+		{
+			DynamicRingBuffer<int> buffer = new(5);
+
+			Assert.ThrowsException<InvalidOperationException>(() => buffer[0]);
+		}
+
+		[TestMethod()]
+		public void IndexerSet_Test()
+		{
+			int[] arr = new int[] { 4, 2, 3 };
+			DynamicRingBuffer<int> buffer = new(arr);
+
+			Assert.AreEqual(2, arr[1]);
+			arr[1] = 5;
+			Assert.AreEqual(5, arr[1]);
+		}
+
+		[TestMethod()]
+		public void IndexerInvalidSet_Test()
+		{
+			DynamicRingBuffer<int> buffer = new(5);
+
+			Assert.ThrowsException<InvalidOperationException>(() => buffer[0] = 5);
+		}
+	}
+}
