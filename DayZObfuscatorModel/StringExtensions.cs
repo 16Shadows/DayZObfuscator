7567464c809151dacce2a9809918933c83263240
16Shadows <egor.0.00@mail.ru>
2023-12-25 18:02:10 +0500

    Added string extension to escape/unescape string

---
+++ DayZObfuscatorModel/StringExtensions.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace DayZObfuscatorModel
+{
+	public static class StringExtensions
+	{
+        static StringExtensions()
+        {
+            _EscapeSequences = new Dictionary<string, string>()
+            {
+                {@"""", @"\"""},
+                {@"\", @"\\"},
+                {"\a", @"\a"},
+                {"\b", @"\b"},
+                {"\f", @"\f"},
+                {"\n", @"\n"},
+                {"\r", @"\r"},
+                {"\t", @"\t"},
+                {"\v", @"\v"},
+                {"\0", @"\0"},
+            };
+
+            _EscapeMatcher = new Regex(string.Join('|', _EscapeSequences.Keys.Select(Regex.Escape)));
+
+            _UnescapeSequences = _EscapeSequences.ToDictionary(x => x.Value, x => x.Key);
+            _UnescapeMatcher = new Regex(string.Join('|', _UnescapeSequences.Keys.Select(Regex.Escape)));
+        }
+
+		private static Dictionary<string, string> _EscapeSequences;
+        private static Dictionary<string, string> _UnescapeSequences;
+        private static Regex _EscapeMatcher;
+        private static Regex _UnescapeMatcher;
+
+		public static string Escape(this string str)
+		{
+            return _EscapeMatcher.Replace(str, (match) => _EscapeSequences[match.Value] );
+		}
+
+        public static string Unescape(this string str)
+        {
+            return _UnescapeMatcher.Replace(str, (match) => _UnescapeSequences[match.Value] );
+        }
+	}
+}
---
+++ Tests_DayZObfuscatorModel/StringExtensions_Tests.cs
@@ -0,0 +1,155 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DayZObfuscatorModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace DayZObfuscatorModel.Tests
+{
+	[TestClass()]
+	public class StringExtensions_Tests
+	{
+		[TestMethod()]
+		public void Escape_Quotes_Test()
+		{
+			string sample = "\"Hello, world!\"";
+			Assert.AreEqual( @"\""Hello, world!\""", sample.Escape() );
+		}
+
+		[TestMethod()]
+		public void Unescape_Quotes_Test()
+		{
+			string sample = "\"Hello, world!\"";
+			Assert.AreEqual(sample, sample.Escape().Unescape() );
+		}
+
+		[TestMethod()]
+		public void Escape_Backslash_Test()
+		{
+			string sample = @"Hello, world\friend!";
+			Assert.AreEqual( @"Hello, world\\friend!", sample.Escape() );
+		}
+
+		[TestMethod()]
+		public void Unescape_Backslash_Test()
+		{
+			string sample = @"Hello, world\friend!";
+			Assert.AreEqual(sample, sample.Escape().Unescape() );
+		}
+
+		[TestMethod()]
+		public void Escape_Alert_Test()
+		{
+			string sample = "\aHello, world!";
+			Assert.AreEqual( @"\aHello, world!", sample.Escape() );
+		}
+
+		[TestMethod()]
+		public void Unescape_Alert_Test()
+		{
+			string sample = "\aHello, world!";
+			Assert.AreEqual(sample, sample.Escape().Unescape() );
+		}
+
+		[TestMethod()]
+		public void Escape_Backspace_Test()
+		{
+			string sample = "\bHello, world!";
+			Assert.AreEqual( @"\bHello, world!", sample.Escape() );
+		}
+
+		[TestMethod()]
+		public void Unescape_Backspace_Test()
+		{
+			string sample = "\bHello, world!";
+			Assert.AreEqual(sample, sample.Escape().Unescape() );
+		}
+
+		[TestMethod()]
+		public void Escape_FormFeed_Test()
+		{
+			string sample = "\fHello, world!";
+			Assert.AreEqual( @"\fHello, world!", sample.Escape() );
+		}
+
+		[TestMethod()]
+		public void Unescape_FormFeed_Test()
+		{
+			string sample = "\fHello, world!";
+			Assert.AreEqual(sample, sample.Escape().Unescape() );
+		}
+
+		[TestMethod()]
+		public void Escape_NewLine_Test()
+		{
+			string sample = "\nHello, world!";
+			Assert.AreEqual( @"\nHello, world!", sample.Escape() );
+		}
+
+		[TestMethod()]
+		public void Unescape_NewLine_Test()
+		{
+			string sample = "\nHello, world!";
+			Assert.AreEqual(sample, sample.Escape().Unescape() );
+		}
+
+		[TestMethod()]
+		public void Escape_CarriageReturn_Test()
+		{
+			string sample = "\rHello, world!";
+			Assert.AreEqual( @"\rHello, world!", sample.Escape() );
+		}
+
+		[TestMethod()]
+		public void Unescape_CarriageReturn_Test()
+		{
+			string sample = "\rHello, world!";
+			Assert.AreEqual(sample, sample.Escape().Unescape() );
+		}
+
+		[TestMethod()]
+		public void Escape_HorizontalTab_Test()
+		{
+			string sample = "\tHello, world!";
+			Assert.AreEqual( @"\tHello, world!", sample.Escape() );
+		}
+
+		[TestMethod()]
+		public void Unescape_HorizontalTab_Test()
+		{
+			string sample = "\tHello, world!";
+			Assert.AreEqual(sample, sample.Escape().Unescape() );
+		}
+
+		[TestMethod()]
+		public void Escape_VerticalTab_Test()
+		{
+			string sample = "\vHello, world!";
+			Assert.AreEqual( @"\vHello, world!", sample.Escape() );
+		}
+
+		[TestMethod()]
+		public void Unescape_VerticalTab_Test()
+		{
+			string sample = "\vHello, world!";
+			Assert.AreEqual(sample, sample.Escape().Unescape() );
+		}
+
+		[TestMethod()]
+		public void Escape_Complex_Test()
+		{
+			string sample = "\"\aHello,\n\r\t\vworld\\friend!\b\f\"";
+			Assert.AreEqual( @"\""\aHello,\n\r\t\vworld\\friend!\b\f\""", sample.Escape() );
+		}
+
+		[TestMethod()]
+		public void Unescape_Complex_Test()
+		{
+			string sample = "\"\aHello,\n\r\t\vworld\\friend!\b\f\"";
+			Assert.AreEqual(sample, sample.Escape().Unescape() );
+		}
+	}
+}
