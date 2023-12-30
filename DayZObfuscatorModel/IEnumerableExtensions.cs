3fbeb7710b144ff1bffff969a9f66d5c13a77442
16Shadows <egor.0.00@mail.ru>
2023-12-30 16:56:29 +0500

    Added IEnumerable<T> extension method SequenceEqualsOrderInvariant which compares to sequences for equality disregarding the order of items.

---
+++ DayZObfuscatorModel/IEnumerableExtensions.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DayZObfuscatorModel
+{
+	public static class IEnumerableExtensions
+	{
+		public static bool SequenceEqualsOrderInvariant<T>(this IEnumerable<T> first, IEnumerable<T> second) => SequenceEqualsOrderInvariant(first, second, EqualityComparer<T>.Default);
+
+		public static bool SequenceEqualsOrderInvariant<T>(this IEnumerable<T> first, IEnumerable<T> second, IEqualityComparer<T> equalityComparer)
+		{
+			//Code analyzer complains here because TKey shouldn't be nullable (makes sense). However, we need to allow T to be nullable. We are never actually storing nulls in the dictionary.
+#pragma warning disable CS8714
+			Dictionary<T, int> counters = new Dictionary<T, int>(equalityComparer);
+#pragma warning restore CS8714
+			int nullCounter = 0;
+
+			int count = 0;
+
+			foreach (var item in first)
+			{
+				if (item is null)
+					nullCounter++;
+				else if (counters.TryGetValue(item, out count))
+					counters[item] = count + 1;
+				else
+					counters.Add(item, 1);
+			}
+
+			foreach (var item in second)
+			{
+				if (item is null)
+					nullCounter--;
+				else if (counters.TryGetValue(item, out count))
+				{
+					if (count == 1)
+						counters.Remove(item);
+					else
+						counters[item] = count - 1;
+				}
+				else
+					counters.Add(item, -1);
+			}
+
+			return nullCounter == 0 && counters.Count == 0;
+		}
+	}
+}
---
+++ Tests_DayZObfuscatorModel/IEnumerableExtensions_Tests.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DayZObfuscatorModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics.CodeAnalysis;
+
+namespace DayZObfuscatorModel.Tests
+{
+	[TestClass()]
+	public class IEnumerableExtensions_Tests
+	{
+		[TestMethod()]
+		public void SequenceEqualsOrderInvariant_ValidTest()
+		{
+			List<int> a = new List<int>() { 2, 3, 4, 5, 5, 4 };
+			List<int> b = new List<int>() { 5, 4, 3, 2, 4, 5 };
+
+			Assert.IsTrue(a.SequenceEqualsOrderInvariant(b));
+			Assert.IsTrue(b.SequenceEqualsOrderInvariant(a));
+		}
+
+		[TestMethod()]
+		public void SequenceEqualsOrderInvariant_InvalidTest()
+		{
+			List<int> a = new List<int>() { 2, 3, 4, 4, 4, 4 };
+			List<int> b = new List<int>() { 5, 4, 3, 2, 4, 5 };
+
+			Assert.IsFalse(a.SequenceEqualsOrderInvariant(b));
+			Assert.IsFalse(b.SequenceEqualsOrderInvariant(a));
+		}
+
+		[TestMethod()]
+		public void SequenceEqualsOrderInvariant_InvalidTest2()
+		{
+			List<int> a = new List<int>() { 2, 3, 4, 4, 4, 4, 5, 4 };
+			List<int> b = new List<int>() { 5, 4, 3, 2, 4, 5 };
+
+			Assert.IsFalse(a.SequenceEqualsOrderInvariant(b));
+			Assert.IsFalse(b.SequenceEqualsOrderInvariant(a));
+		}
+
+		private class TestEqualityComparer : IEqualityComparer<int>
+		{
+			public bool Equals(int x, int y)
+			{
+				return true;
+			}
+
+			public int GetHashCode([DisallowNull] int obj)
+			{
+				return 0;
+			}
+		}
+
+		[TestMethod()]
+		public void SequenceEqualsOrderInvariant_EqualityComparer_ValidTest()
+		{
+			List<int> a = new List<int>() { 2, 3, 4, 5, 5, 4 };
+			List<int> b = new List<int>() { 5, 4, 3, 2, 4, 5 };
+
+			Assert.IsTrue(a.SequenceEqualsOrderInvariant(b, new TestEqualityComparer()));
+			Assert.IsTrue(b.SequenceEqualsOrderInvariant(a, new TestEqualityComparer()));
+		}
+
+		[TestMethod()]
+		public void SequenceEqualsOrderInvariant_EqualityComparer_ValidTest2()
+		{
+			List<int> a = new List<int>() { 2, 3, 4, 5, 5, 4 };
+			List<int> b = new List<int>() { 5, 4, 3, 2, 7, 6 };
+
+			Assert.IsTrue(a.SequenceEqualsOrderInvariant(b, new TestEqualityComparer()));
+			Assert.IsTrue(b.SequenceEqualsOrderInvariant(a, new TestEqualityComparer()));
+		}
+
+		[TestMethod()]
+		public void SequenceEqualsOrderInvariant_EqualityComparer_InvalidTest()
+		{
+			List<int> a = new List<int>() { 2, 3, 4, 5, 5, 4 };
+			List<int> b = new List<int>() { 5, 4, 3, 2, 4, 5, 7, 6 };
+
+			Assert.IsFalse(a.SequenceEqualsOrderInvariant(b, new TestEqualityComparer()));
+			Assert.IsFalse(b.SequenceEqualsOrderInvariant(a, new TestEqualityComparer()));
+		}
+	}
+}
