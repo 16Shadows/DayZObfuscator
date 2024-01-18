610d7759cad17df651dd58e6ecdd2c68b50c4cb6
16Shadows <egor.0.00@mail.ru>
2024-01-18 17:12:02 +0500

    Added FileInputReader which feeds text to lexer from a file.

---
+++ DayZObfuscatorModel/Parser/FileInputReader.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DayZObfuscatorModel.Parser
+{
+	public class FileInputReader : IInputReader
+	{
+		protected readonly DynamicRingBuffer<char> _InputBuffer;
+		protected readonly StreamReader _InputStream;
+
+		public FileInputReader(string path)
+		{
+			ArgumentNullException.ThrowIfNull(path);
+
+			if (!Path.Exists(path) || Path.GetFileName(path) == null)
+				throw new ArgumentException($"{nameof(path)} should be a path to an existing file.");
+
+			_InputBuffer = new DynamicRingBuffer<char>(50);
+			_InputStream = File.OpenText(path);
+		}
+
+		public char Consume()
+		{
+			if ( _InputBuffer.Count < 1 && Preread(1) < 1)
+				return '\0';
+
+			return _InputBuffer.Pop();
+		}
+
+		public string Consume(int count)
+		{
+			if ( _InputBuffer.Count < count)
+			{
+				int toRead = count - _InputBuffer.Count;
+				int read = Preread(toRead);
+
+				if (read < toRead)
+					return string.Join("", _InputBuffer.Pop(_InputBuffer.Count).Concat(Enumerable.Repeat('\0', toRead - read)));
+			}
+
+			return string.Join("", _InputBuffer.Pop(count));
+		}
+
+		public char Peek()
+		{
+			if ( _InputBuffer.Count < 1 && Preread(1) < 1)
+				return '\0';
+			else
+				return _InputBuffer[0];
+		}
+
+		public string Peek(int count)
+		{
+			if ( _InputBuffer.Count < count)
+			{
+				int toRead = count - _InputBuffer.Count;
+				int read = Preread(toRead);
+
+				if (read < toRead)
+					return string.Join("", _InputBuffer.Concat(Enumerable.Repeat('\0', toRead - read)));
+			}
+
+			return string.Join("", _InputBuffer.Take(count));
+		}
+
+		protected int Preread(int count)
+		{
+			int totalRead = 0, lastRead;
+
+			char[] buffer = new char[50];
+
+			for (int i = 0; i < count / 50; i++)
+			{
+				lastRead = _InputStream.Read(buffer, 0, 50);
+
+				totalRead += lastRead;
+				
+				if (lastRead > 0)
+					_InputBuffer.AddRange(buffer);
+
+				if (lastRead < 50)
+					return totalRead;
+			}
+
+			count %= 50;
+			if (count > 0)
+			{
+				lastRead = _InputStream.Read(buffer, 0, count);
+				
+				totalRead += lastRead;
+
+				if (lastRead > 0)
+					_InputBuffer.AddRange(buffer.Take(count));
+			}
+			
+			return totalRead;
+		}
+	}
+}
---
+++ Tests_DayZObfuscatorModel/Parser/FileInputReader_Tests.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DayZObfuscatorModel.Parser;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+
+namespace DayZObfuscatorModel.Parser.Tests
+{
+	[TestClass()]
+	public class FileInputReader_Tests
+	{
+		protected static string TestFileContent =	"class CfgMods\n" +
+													"{\n" +
+													"\tclass SuicideButton\n" +
+													"\t{\n" +
+													"\t\tdir=\"SuicideButton\";\n" +
+													"\t\ttype = \"mod\";\n" +
+													"\t\tauthor = \"16Shadows\";\n" +
+													"\t\tversion = \"1.0.0\";\n" +
+													"\t\tname=\"SuicideButton\";\n" +
+													"\t\tdependencies[] = {\"Mission\"};\n" +
+													"\t\tclass defs\n" +
+													"\t\t{\n" +
+													"\t\t\tclass missionScriptModule\n" +
+													"\t\t\t{\n" +
+													"\t\t\t\tvalue = \"\";\n" +
+													"\t\t\t\tfiles[] = {\"SuicideButton/5_Mission\" };\n" +
+													"\t\t\t};\n" +
+													"\t\t};\n" +
+													"\t};\n" +
+													"};";
+
+		protected void GenerateTestFile([CallerMemberName]string caller="")
+		{
+			string path = GetTestFileName(caller);
+
+			if (File.Exists(path))
+				File.Delete(path);
+
+			Directory.CreateDirectory(Path.GetDirectoryName(path));
+			File.WriteAllText(path, TestFileContent);
+		}
+
+		protected string GetTestFileName([CallerMemberName]string caller="")
+		{
+			return $"FileInputReader_Tests/{caller}/config.cpp";
+		}
+
+		[TestMethod()]
+		public void Consume_Test()
+		{
+			GenerateTestFile();
+
+			FileInputReader reader = new FileInputReader(GetTestFileName());
+
+			Assert.AreEqual(TestFileContent[0], reader.Consume());
+			Assert.AreEqual(TestFileContent[1], reader.Consume());
+			Assert.AreEqual(TestFileContent[2], reader.Consume());
+			Assert.AreEqual(TestFileContent[3], reader.Consume());
+			Assert.AreEqual(TestFileContent[4], reader.Consume());
+			Assert.AreEqual(TestFileContent[5], reader.Consume());
+		}
+
+		[TestMethod()]
+		public void ConsumeN_Test()
+		{
+			GenerateTestFile();
+
+			FileInputReader reader = new FileInputReader(GetTestFileName());
+
+			Assert.AreEqual(TestFileContent[0..6], reader.Consume(6));
+		}
+
+		[TestMethod()]
+		public void ConsumePastEnd_Test()
+		{
+			GenerateTestFile();
+
+			FileInputReader reader = new FileInputReader(GetTestFileName());
+
+			reader.Consume(TestFileContent.Length);
+
+			Assert.AreEqual('\0', reader.Consume());
+		}
+
+		[TestMethod()]
+		public void Peek_Test()
+		{
+			GenerateTestFile();
+
+			FileInputReader reader = new FileInputReader(GetTestFileName());
+
+			Assert.AreEqual(TestFileContent[0], reader.Peek());
+			Assert.AreEqual(TestFileContent[0], reader.Peek());
+			Assert.AreEqual(TestFileContent[0], reader.Consume());
+			Assert.AreEqual(TestFileContent[1], reader.Peek());
+		}
+
+		[TestMethod()]
+		public void PeekN_Test()
+		{
+			GenerateTestFile();
+
+			FileInputReader reader = new FileInputReader(GetTestFileName());
+
+			Assert.AreEqual(TestFileContent[0..6], reader.Peek(6));
+			Assert.AreEqual(TestFileContent[0..6], reader.Consume(6));
+			Assert.AreEqual(TestFileContent[6..12], reader.Peek(6));
+		}
+
+		[TestMethod()]
+		public void PeekPastEnd_Test()
+		{
+			GenerateTestFile();
+
+			FileInputReader reader = new FileInputReader(GetTestFileName());
+
+			reader.Consume(TestFileContent.Length);
+
+			Assert.AreEqual('\0', reader.Peek());
+		}
+	}
+}
