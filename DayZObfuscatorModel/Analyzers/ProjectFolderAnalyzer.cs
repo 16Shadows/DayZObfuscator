9e65993bcbaf7b49797a3aff0051424867a860ec
16Shadows <egor.0.00@mail.ru>
2024-01-20 12:16:12 +0500

    Added a method to load single pbo from its root (containing config.cpp) to ProjectFolderAnalyzer.

--- DayZObfuscatorModel/Analyzers/ProjectFolderAnalyzer.cs
+++ DayZObfuscatorModel/Analyzers/ProjectFolderAnalyzer.cs
@@ -2,12 +2,33 @@
 using DayZObfuscatorModel.PBO;
 using DayZObfuscatorModel.PBO.Config.Parser;
 using DayZObfuscatorModel.PBO.Config.Parser.Lexer;
+using System.IO;
 
 namespace DayZObfuscatorModel.Analyzers
 {
 	public static class ProjectFolderAnalyzer
 	{
 		private static readonly ConfigParser _ConfigParser = new ConfigParser( new ConfigParserErrorResolver() );
+
+		public static PBODescriptor LoadPBO(string pathToRoot)
+		{
+			if (!Directory.Exists(pathToRoot))
+				throw new ArgumentException("Path should be a valid path to a directory.", nameof(pathToRoot));
+
+			pathToRoot = Path.GetFullPath(pathToRoot);
+
+			if (!File.Exists(pathToRoot + "\\config.cpp"))
+				throw new ArgumentException("Target directory doesn't contain config.cpp - not a valid PBO directory", nameof(pathToRoot));
+
+			var result = _ConfigParser.Parse( new ConfigLexer( new FileInputReader(pathToRoot + "\\config.cpp") ) );
+
+			var descriptor = new PBODescriptor(pathToRoot, result);
+
+			foreach (PBOFile file in EnumerateFiles(pathToRoot, "").Where(x => x.Filename != "config.cpp"))
+				descriptor.Files.Add(file);
+
+			return descriptor;
+		}
 
 		public static IEnumerable<PBODescriptor> Analyze(string path)
 		{
@@ -39,12 +60,12 @@ namespace DayZObfuscatorModel.Analyzers
 			return descriptors;
 		}
 
-		private static IEnumerable<PBOFile> EnumerateFiles(string path, string relativePath, HashSet<string> dirFilter)
+		private static IEnumerable<PBOFile> EnumerateFiles(string path, string relativePath, HashSet<string>? dirFilter = null)
 		{
 			foreach (string file in Directory.EnumerateFiles(path))
 				yield return new PBOFile(Path.GetFullPath(file), relativePath + (relativePath.Length > 0 ? "/" : "") + Path.GetFileName(file));
 
-			foreach (string subdir in Directory.EnumerateDirectories(path).Where(x => !dirFilter.Contains(x)))
+			foreach (string subdir in Directory.EnumerateDirectories(path).Where(x => dirFilter?.Contains(x) != true))
 				foreach (PBOFile file in EnumerateFiles( subdir, relativePath + Path.GetFileName(subdir), dirFilter ) )
 					yield return file;
 
--- Tests_DayZObfuscatorModel/Analyzers/ProjectFolderAnalyzer_Tests.cs
+++ Tests_DayZObfuscatorModel/Analyzers/ProjectFolderAnalyzer_Tests.cs
@@ -13,6 +13,55 @@ namespace DayZObfuscatorModel.Analyzers.Tests
 	public class ProjectFolderAnalyzer_Tests
 	{
 		[TestMethod()]
+		public void LoadPBO_Test()
+		{
+			if (Directory.Exists("ProjectFolderAnalyzer_Tests/LoadPBO_Test"))
+				Directory.Delete("ProjectFolderAnalyzer_Tests/LoadPBO_Test", true);
+
+			Directory.CreateDirectory("ProjectFolderAnalyzer_Tests/LoadPBO_Test/SuicideButton");
+
+			string config = "class CfgMods\n" +
+							"{\n" +
+							"\tclass SuicideButton\n" +
+							"\t{\n" +
+							"\t\tdir=\"SuicideButton\";\n" +
+							"\t\ttype = \"mod\";\n" +
+							"\t\tauthor = \"16Shadows\";\n" +
+							"\t\tversion = \"1.0.0\";\n" +
+							"\t\tname=\"SuicideButton\";\n" +
+							"\t\tdependencies[] = {\"Mission\"};\n" +
+							"\t\tclass defs\n" +
+							"\t\t{\n" +
+							"\t\t\tclass missionScriptModule\n" +
+							"\t\t\t{\n" +
+							"\t\t\t\tvalue = \"\";\n" +
+							"\t\t\t\tfiles[] = {\"SuicideButton/5_Mission\" };\n" +
+							"\t\t\t};\n" +
+							"\t\t};\n" +
+							"\t};\n" +
+							"};";
+
+			File.WriteAllText("ProjectFolderAnalyzer_Tests/LoadPBO_Test/SuicideButton/config.cpp", config);
+			Directory.CreateDirectory("ProjectFolderAnalyzer_Tests/LoadPBO_Test/SuicideButton/5_Mission");
+
+			string script = "modded class MissionServer\n" + 
+							"{\n" +
+							"\tvoid MissionServer()\n" +
+							"\t{\n" +
+							"\t\tPrint(\"Hello, world!\");\n" +
+							"\t}\n" +
+							"}";
+
+			File.WriteAllText("ProjectFolderAnalyzer_Tests/LoadPBO_Test/SuicideButton/5_Mission/mission.c", script);
+
+			List<PBODescriptor> pbos = ProjectFolderAnalyzer.Analyze("ProjectFolderAnalyzer_Tests/LoadPBO_Test").ToList();
+
+			Assert.AreEqual(1, pbos.Count);
+			Assert.AreEqual(Path.GetFullPath("ProjectFolderAnalyzer_Tests/LoadPBO_Test/SuicideButton"), pbos[0].DirectoryPath);
+			Assert.AreEqual(1, pbos[0].Files.Count);
+		}
+
+		[TestMethod()]
 		public void Analyze_SingleProject_Test()
 		{
 			if (Directory.Exists("ProjectFolderAnalyzer_Tests/Analyze_SingleProject_Test"))
