65bb6fbf779be0ca18c96b4a490e6aa9be0b19ea
16Shadows <egor.0.00@mail.ru>
2024-02-10 16:33:10 +0500

    Added customizable Escape/Deescape methods.

--- DayZObfuscatorModel/StringExtensions.cs
+++ DayZObfuscatorModel/StringExtensions.cs
@@ -38,14 +38,45 @@ namespace DayZObfuscatorModel
         private static Regex _EscapeMatcher;
         private static Regex _UnescapeMatcher;
 
+        /// <summary>
+        /// Escapes this string using default escape table
+        /// </summary>
+        /// <param name="str">This string</param>
+        /// <returns>Escaped string</returns>
 		public static string Escape(this string str)
 		{
             return _EscapeMatcher.Replace(str, (match) => _EscapeSequences[match.Value] );
 		}
 
+        /// <summary>
+        /// Unescape this string using default escape table
+        /// </summary>
+        /// <param name="str">This string</param>
+        /// <returns>Unescaped string</returns>
         public static string Unescape(this string str)
         {
             return _UnescapeMatcher.Replace(str, (match) => _UnescapeSequences[match.Value] );
         }
+
+        /// <summary>
+        /// Escape this string using custom escape table
+        /// </summary>
+        /// <param name="str">This string</param>
+        /// <param name="escapeTable">Escape table</param>
+        /// <returns>Escaped string</returns>
+        public static string Escape(this string str, Dictionary<string, string> escapeTable)
+		{
+            Regex matcher = new Regex(string.Join("|", escapeTable.Keys.Select(Regex.Escape)));
+            return matcher.Replace(str, (match) => escapeTable[match.Value] );
+		}
+
+        /// <summary>
+        /// Unescapes this string using default escape table (this table is the same which is used in <see cref="Escape(string, Dictionary{string, string})"/>, it will be automatically reversed).
+        /// </summary>
+        /// <param name="str">This string</param>
+        /// <param name="escapeTable">Escape table (same as in <see cref="Escape(string, Dictionary{string, string})"/>, it will be automatically reversed).</param>
+        /// <returns>Unescaped string</returns>
+        public static string Unescape(this string str, Dictionary<string, string> escapeTable) =>
+            Escape(str, escapeTable.ToDictionary(x => x.Value, x => x.Key));
 	}
 }
