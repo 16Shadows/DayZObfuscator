2e1f8b97f485ca0ebdbac678efbd1e1d6b065154
16Shadows <egor.0.00@mail.ru>
2024-02-10 16:35:17 +0500

    Added PBOConfigValueUnescapedString - a version of PBOValueString which does not character escaping except for quotes when being converted to string.

---
+++ DayZObfuscatorModel/PBO/Config/PBOConfigValueUnescapedString.cs
@@ -0,0 +1,42 @@
+﻿namespace DayZObfuscatorModel.PBO.Config
+{
+	public class PBOConfigValueUnescapedString : IEquatable<PBOConfigValueString>, IEquatable<string>
+	{
+		private static readonly Dictionary<string, string> _EscapeTable = new Dictionary<string, string>() { {@"""", @"\"""} };
+
+		private string _Value;
+
+		public string Value { get => _Value; set => _Value = value ?? throw new ArgumentNullException(nameof(value)); }
+
+		public PBOConfigValueUnescapedString(string value)
+		{
+			_Value = value ?? throw new ArgumentNullException(nameof(value));
+		}
+
+		public override string ToString()
+		{
+			return $"\"{Value.Escape(_EscapeTable)}\"";
+		}
+
+		public override bool Equals(object? obj)
+		{
+			return (obj is string str && Equals(str)) ||
+				   (obj is PBOConfigValueString other && Equals(other));
+		}
+
+		public override int GetHashCode()
+		{
+			return HashCode.Combine(Value);
+		}
+
+		public bool Equals(PBOConfigValueString? other)
+		{
+			return Value.Equals(other?.Value);
+		}
+
+		public bool Equals(string? other)
+		{
+			return Value.Equals(other);
+		}
+	}
+}
